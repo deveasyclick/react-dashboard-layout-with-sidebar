43dfe06c7619a326d1f4e83d26a6cf59540807ec
Yusuf <easyclick06@gmail.com>
2025-04-13 18:19:03 +0100

    * Hide sidebar toggle for md screen down

--- src/components/Headers/main/Main.tsx
+++ src/components/Headers/main/Main.tsx
@@ -61,7 +61,7 @@ const MainHeader = ({
     <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
       <div className="flex flex-wrap justify-between items-center">
         <section className="flex justify-start items-center">
-          <HeaderButton ariaControls="sidebar">
+          <HeaderButton ariaControls="sidebar" className="md:hidden">
             <IconWrapper name="menu" />
             <IconWrapper name="cancel" aria-hidden="true" className="hidden" />
             <span className="sr-only">Toggle sidebar</span>
